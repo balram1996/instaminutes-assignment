7cccec6e1a9461c5df075b2e5d55a27c0e985345
balram1996 <kurmibalram936@gmail.com>
2021-12-25 15:46:27 +0530

    setting message and user done

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,49 @@
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+
+
+//Get username and room from URL
+const{ username,room} = Qs.parse(location.search , {
+    ignoreQueryPrefix :true
+})
+
 const socket = io();
+
+//Join chat room
+socket.emit("joinRoom" , {username,room})
+
+//Message from server
+socket.on("message", (message) => {
+  console.log(message);
+  outputMessage(message);
+
+  //scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+//Message submit
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  //get message text
+  const msg = e.target.elements.msg.value;
+
+  //emitting a message to server
+  socket.emit("chatMessage", msg);
+
+  //clear input after setting message to the server
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
+});
+
+
+//Output message to DOM
+function outputMessage(message) {
+    const div = document.createElement("div");
+    div.classList.add("message");
+    div.innerHTML = `<p class="meta">${message.username}<span>${message.time}</span></p>
+    <p class="text">
+        ${message.text}
+    </p>`;
+    document.querySelector(".chat-messages").appendChild(div)
+}
--- server.js
+++ server.js
@@ -5,15 +5,39 @@ const socketio = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+const formatMessage = require("./utils/messages");
 
 //set static folder
 app.use(express.static(path.join(__dirname, "public")))
-const PORT = 3000 || process.env.PORT;
+
+const adminName = "Chat App"
 
 //run when client connect
 io.on("connection" , socket=>{
-    console.log("new web socket connection")
-})
+    socket.on("joinRoom" , ({username,room})=>{
+     
+        //welcome current user
+    socket.emit('message' ,formatMessage(adminName,"welcome to chatapp"));
+
+    //Broadcast when a user connects
+    socket.broadcast.emit("message" ,formatMessage(adminName,"A user has joined the chat"));
+
+    });
+
+    //listen chat messages
+    socket.on("chatMessage" , msg=>{
+        io.emit("message" ,formatMessage("USER",msg))
+       console.log(msg)
+    });
+
+    //Runs when client disconnects
+    socket.on("disconnect" , ()=>{
+        io.emit("message" , formatMessage(adminName,"A user has left the chat"))
+    });
+
+});
+
+const PORT = 3000 || process.env.PORT;
 server.listen(PORT , ()=>{
     console.log(`server running on port ${PORT}`)
 });
